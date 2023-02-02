af28917a8699eff80ac6f03b89f8801fbb69a5aa
choi yuri <yurifrom1123@gmail.com>
2023-02-02 09:58:54 +0900

    commit

---
+++ src/main/java/com/yuri/shoppingsite/ShoppingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.shoppingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingsiteApplication.class, args);
+    }
+
+}
