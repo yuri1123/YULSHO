6671daf959a70430fc2d59bb3f350ae5bdf39e89
choi yuri <yurifrom1123@gmail.com>
2023-02-14 18:05:52 +0900

    commit

---
+++ src/main/java/com/yuri/shoppingsite/domain/Products.java
@@ -0,0 +1,28 @@
+package com.yuri.shoppingsite.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique=true)
+    private String category;
+    @Column(unique = true)
+    private String productname;
+    private String productcontent;
+    private String price;
+    private String etc;
+    private String regdate;
+
+
+
+}
