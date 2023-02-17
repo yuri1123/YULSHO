eb7cbea566fcaefa72113ad81bdb6a39978a1584
choi yuri <yurifrom1123@gmail.com>
2023-02-17 17:45:04 +0900

    commit

--- src/main/java/com/yuri/shoppingsite/controller/ShoppingController.java
+++ src/main/java/com/yuri/shoppingsite/controller/ShoppingController.java
@@ -1,15 +1,10 @@
 package com.yuri.shoppingsite.controller;
 
-import com.yuri.shoppingsite.domain.shop.Products;
 import com.yuri.shoppingsite.service.ProductService;
 import com.yuri.shoppingsite.service.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Component
 @Controller
@@ -17,9 +12,7 @@ public class ShoppingController {
 
 
     private ProductService productService;
-    private String uploadDir;
 
-    private UserService userService;
 
     //상품 전체 보여주기 리스트로 가기
     @GetMapping("shopping/shopping")
@@ -34,16 +27,16 @@ public class ShoppingController {
 
     //상품 등록 페이지로 가기(insert)
     @GetMapping("shopping/uploadproduct")
-    public void uploadProductsP(Model model) {
+    public void uploadProductsP() {
     }
 
     //상품 등록(image 파일 추가)
-    @PostMapping("shopping/uploadproduct")
-    public String uploadProduct(Products products){
-     productService.insert(products.getCategory(),products.getFilename()
-     ,products.getProductcontent(),products.getPrice(),products.getQuantity(),products.getEtc(),
-             products.getUuid(),products.getFilename(),products.getRegDate());
-
-        return "/shopping/productlist";
-    }
+//    @PostMapping("shopping/uploadproduct")
+//    public String uploadProduct(Products products){
+//     productService.insert(products.getCategory(),products.getFilename()
+//     ,products.getProductcontent(),products.getPrice(),products.getQuantity(),products.getEtc(),
+//             products.getUuid(),products.getFilename(),products.getRegDate());
+//
+//        return "/shopping/productlist";
+//    }
 }
